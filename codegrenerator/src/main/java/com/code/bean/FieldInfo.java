b76397ea322bfa0c5a0ba78426c07be37d104f79
yangkaihu <yangkaihu@yeah.net>
2024-05-04 20:08:50 +0800

    read tables infomation

---
+++ codegrenerator/src/main/java/com/code/bean/FieldInfo.java
@@ -0,0 +1,40 @@
+package com.code.bean;
+
+
+import lombok.Data;
+
+/**
+ * @author yangkaihu@yeah.net
+ * @date 2024/5/4 17:44
+ */
+
+
+@Data
+public class FieldInfo {
+    /**
+     * 字段名
+     */
+    private String FieldName;
+    /**
+     * bean 属性名称
+     */
+    private String propertyName;
+    /**
+     *对应SQL 类型
+     */
+    private String sqlType;
+    /**
+     * 对应Java字段类型
+     */
+    private String javaType;
+    /**
+     * 字段备注
+     */
+    private String comment;
+    /**
+     * 字段备注是否自增长
+     */
+    private Boolean isAadIncrement;
+
+
+}
