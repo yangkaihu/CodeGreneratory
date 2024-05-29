a0711f261739d66bb95b50a8b35738052db779c5
yangkaihu <yangkaihu@yeah.net>
2024-05-29 09:47:31 +0800

    create po file:v1.0.2

--- codejava-demo/src/main/java/com/codejava/entity/po/UserInfo.java
+++ codejava-demo/src/main/java/com/codejava/entity/po/UserInfo.java
@@ -1,73 +1,72 @@
 package com.codejava.entity.po;
-
 import java.io.Serializable;
-import java.util.Date;
+import java.util.Date; 
 
 /**
- * @Author: 杨开虎
- * @Description: 用户者信息
- * @Date: 2024/05/27
- */
-public class UserInfo implements Serializable {
-    /**
-     * Id
-     */
-    private Integer id;
+*@Author: 杨开虎
+*@Description: 用户者信息
+*@Date: 2024/05/28
+*/
+public class UserInfo  implements   Serializable {
+	 /**
+Id
+	 */
+private Integer id;
 
-    /**
-     * 用户ID
-     */
-    private String userId;
+	 /**
+用户ID
+	 */
+private String userId;
 
-    /**
-     * 昵称
-     */
-    private String nickName;
+	 /**
+昵称
+	 */
+private String nickName;
 
-    /**
-     * 邮箱
-     */
-    private String email;
+	 /**
+邮箱
+	 */
+private String email;
 
-    /**
-     * QQ登录ID
-     */
-    private String qqOpenId;
+	 /**
+QQ登录ID
+	 */
+private String qqOpenId;
 
-    /**
-     * QQ头像
-     */
-    private String qqAvatar;
+	 /**
+QQ头像
+	 */
+private String qqAvatar;
 
-    /**
-     * 密码
-     */
-    private String password;
+	 /**
+密码
+	 */
+private String password;
 
-    /**
-     * 加入时间
-     */
-    private Date joinTime;
+	 /**
+加入时间
+	 */
+private Date joinTime;
 
-    /**
-     * 最后登录时间
-     */
-    private Date lastLoginTime;
+	 /**
+最后登录时间
+	 */
+private Date lastLoginTime;
 
-    /**
-     * 状态
-     */
-    private Integer status;
+	 /**
+状态
+	 */
+private Integer status;
 
-    /**
-     * 用户空间
-     */
-    private Long useSpace;
+	 /**
+用户空间
+	 */
+private Long useSpace;
 
-    /**
-     * 总空间
-     */
-    private Long totalSpace;
+	 /**
+总空间
+	 */
+private Long totalSpace;
 
 
 }
